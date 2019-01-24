99eec3d67b8a4ed8dabd2069b179084d9ce53e8a
Satasuk Viparksinlapin <satasuk@gmail.com>
2019-01-24 21:45:51 +0700

    changing some search filters

--- src/components/Table/QueryTabel/QueryTable.js
+++ src/components/Table/QueryTabel/QueryTable.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react';
 
 import classes from './QueryTable.module.css';
 import '../Table.css';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import 'react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css';
+
 
 class QueryTable extends Component {
+
     state = {
         columns: [{
             dataField: 'key', //concat showroomID with receiveDate(ไม่ก็ใช้ running key มาเปน ID)
@@ -13,10 +17,12 @@ class QueryTable extends Component {
         }, {
             dataField: 'showroomID',
             text: 'รหัสโชว์รูม',
+            filter: textFilter({style:{width:'100px'}, placeholder:'รหัสโชว์รูม'}),
             sort: true
         }, {
             dataField: 'showroomName',
             text: 'ชื่อโชว์รูม',
+            filter: textFilter({style:{width:'120px'}, placeholder:'ชื่อโชว์รูม'}),
             sort: true
         }, {
             dataField: 'receiveDate',
@@ -28,9 +34,11 @@ class QueryTable extends Component {
             sort: true
         }, {
             dataField: 'status',
+            filter: textFilter({style:{width:'75px'}}),
             text: 'Status',
         }, {
             dataField: 'saleName',
+            filter: textFilter({style:{width:'150px'}, placeholder:'ชื่อ Sale'}),
             text: 'ชื่อ Sale',
         }],
 
@@ -48,8 +56,9 @@ class QueryTable extends Component {
                     hover
                     keyField='key'
                     data={this.props.rows}
-                    columns={this.state.columns} 
-                    rowEvents={this.props.events} />
+                    columns={this.state.columns}
+                    rowEvents={this.props.events}
+                    filter={filterFactory()} />
             </div>
         );
 
